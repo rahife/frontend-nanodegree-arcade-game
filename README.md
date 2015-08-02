c58d0afdaa1bedb0b0c6c255f155857f34505c72
rafa.hidalgo <rafa.hidalgo@apiumtech.com>
2015-08-02 19:11:38 +0700

    second commit

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ frontend-nanodegree-arcade-game
 Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
 
 for self-checking their submission.
+
+Notes:
+
+Review the code and comments provided in app.js
+Identify the various classes you will need to write.
+Identify and code the properties each class must have to accomplish its tasks.
+Write the functions that provide functionality to each of your class instances.
--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(speed, x , y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+    this.speed = speed;
+    this.x = x;
+    this.y = y;
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,22 +17,58 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-}
+    if (this.x > 500){
+        this.x = 1;
+    }
+    else this.x = this.x + (this.speed*dt);
+    console.log('enemy!!!!!');
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (){
+    this.sprite = 'images/char-boy.png';
+    this.x = 2*100;
+    this.y = 4*101;
+};
 
+Player.prototype.update = function(dt){
+    console.log('player!!!!');
+};
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(movement){
+    switch(movement) {
+        case 'left':
+            this.x = this.x - 101;
+            break;
+        case 'up':
+            this.y = this.y - 83;
+            break;
+        case 'right':
+            this.x = this.x + 101;
+            break;
+        case 'down':
+            this.y = this.y + 83;
+            break;
+        default:
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy(100,1,42), new Enemy(150, 1, 124),new Enemy(110,1,206)];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
